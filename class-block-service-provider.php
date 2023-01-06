91694f3f7f4994b2f3649eb0b9267ea4d44fd645
mantle-ci <mantle@alley.com>
2023-01-06 03:29:07 +0000

    Merge pull request #357 from alleyinteractive/application-isolation

---
+++ class-block-service-provider.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Block_Service_Provider class file
+ *
+ * @package Mantle
+ */
+
+namespace Mantle\Blocks;
+
+use Mantle\Blocks\Discover_Blocks;
+use Mantle\Support\Service_Provider;
+
+/**
+ * Block Service Provider
+ *
+ * Provides the foundation for building Gutenberg blocks with Mantle.
+ */
+class Block_Service_Provider extends Service_Provider {
+	/**
+	 * Register the application's blocks.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->app->booting(
+			fn () => collect( $this->get_blocks() )->each(
+				fn ( string $block ) => ( new $block() )->register()
+			)
+		);
+	}
+
+	/**
+	 * Discover blocks for the application.
+	 *
+	 * @return array An array of block class names.
+	 */
+	public function get_blocks(): array {
+		return collect( $this->discover_blocks_within() )
+			->reject( fn ( $dir ) => ! is_dir( $dir ) )
+			->reduce(
+				fn ( array $discovered, string $directory ) => array_merge_recursive(
+					$discovered,
+					Discover_Blocks::within( $directory, $this->app->get_base_path() )
+				),
+				[]
+			);
+	}
+
+	/**
+	 * The array of locations to discover blocks within.
+	 *
+	 * @return array
+	 */
+	protected function discover_blocks_within(): array {
+		return [
+			$this->app->get_app_path( 'blocks' ),
+		];
+	}
+}
